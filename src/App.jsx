a94b37af72f094967ecc45f70f0dd143c7040538
antonio <antoniocarmona126@gmail.com>
2023-09-29 22:13:16 +0300

    Sections Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 const App = () => {
-  <main className="relative">
-    Nav
-    <p>vftdgfhfhjb</p>
-    <section className="xl:padding-1 ">Hero</section>
-  </main>;
+  return (
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding">PopularProducts</section>
+      <section className="padding">SuperQuality</section>
+      <section className="padding-x py-10">Services</section>
+      <section className="padding">SpecialOfferrs</section>
+      <section className="padding bg-pale-blue">CustomerReviews</section>
+      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+      <section className="padding bg-black padding-x padding-t pb-8 text-fuchsia-600">
+        Footer
+      </section>
+    </main>
+  );
 };
 export default App;
